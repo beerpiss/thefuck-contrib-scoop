dcf864c564bc2bef130a473f35bcf7a68d7e2c2f
beerpiss <lacvtg.a1.2023@gmail.com>
2022-06-06 16:28:12 +0700

    feat: scoop_config_remove

---
+++ thefuck_contrib_scoop/rules/scoop_config_remove.py
@@ -0,0 +1,13 @@
+from thefuck.utils import for_app
+from thefuck_contrib_scoop.scoop import scoop_available
+
+@for_app("scoop", at_least=3)
+def match(command):
+    return 'scoop config remove' in command.script
+
+
+def get_new_command(command):
+    return f"scoop config rm remove && scoop config rm {command.script_parts[-1]}"
+
+
+enable_by_default = scoop_available
